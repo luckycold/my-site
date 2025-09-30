5356e8024d749f9c950d94589819e3836d658f14
Luke D Williams <github@luckycold.aleeas.com>
2025-09-30 12:03:19 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/luckycold/my-site",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Discord Community": "https://discord.gg/7UGcvKzBeP",
     },
   }),
 }
